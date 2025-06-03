32fed3fe2ed2e61f589fc2689169a41e2b3dde74
Watchcoin Ventures <156217571+K1mc4n@users.noreply.github.com>
2025-06-04 05:09:44 +0700

    Update Watchcoin.jsx

--- src/Watchcoin.jsx
+++ src/Watchcoin.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "./components/ui/card";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
 
